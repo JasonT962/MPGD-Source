9752e40bf8b4bf2a65fc783242494002933648c6
JasonT962 <78263363+JasonT962@users.noreply.github.com>
2022-10-27 00:28:12 +0100

    Added game files for playtest version

---
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -0,0 +1,326 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryManager : MonoBehaviour
+{
+    [SerializeField] private GameObject slotHolder;
+    [SerializeField] private GameObject hotbarSlotHolder;
+
+    [SerializeField] private ItemClass itemToAdd;
+    [SerializeField] private ItemClass itemToRemove;
+
+    [SerializeField] private GameObject cursorIcon;
+
+    [SerializeField] private SlotClass[] startingItems;
+    [SerializeField] private SlotClass[] items;
+
+    private GameObject[] slots;
+    private GameObject[] hotbarSlots;
+
+    private SlotClass originalSlot;
+    private SlotClass movingSlot;
+    private SlotClass tempSlot;
+    private bool itemMoving;
+
+    [SerializeField] private GameObject hotbarSelector;
+    [SerializeField] private int selectedSlot = 0;
+    public ItemClass selectedItem;
+
+    // For enabling and disabling camera / inventory gui
+    public bool inventoryOpen = false;
+    [SerializeField] private GameObject inventoryPanel;
+
+    private void Start()
+    {
+        inventoryPanel.SetActive(false);
+
+        slots = new GameObject[slotHolder.transform.childCount];
+        items = new SlotClass[slots.Length];
+
+        hotbarSlots = new GameObject[hotbarSlotHolder.transform.childCount];
+        for (int i = 0; i < hotbarSlots.Length; i++)
+        {
+            hotbarSlots[i] = hotbarSlotHolder.transform.GetChild(i).gameObject;
+        }
+
+        // Set all slots
+        for (int i = 0; i < items.Length; i++)
+        {
+            items[i] = new SlotClass();
+        }
+        for (int i = 0; i < startingItems.Length; i++)
+        {
+            items[i] = startingItems[i];
+        }
+        for (int i = 0; i < slotHolder.transform.childCount; i++)
+        {
+            slots[i] = slotHolder.transform.GetChild(i).gameObject;
+        }
+
+        RefreshGUI();
+        if (itemToAdd != null)
+        {
+            Add(itemToAdd,1);
+        }
+        Remove(itemToRemove);
+    }
+
+    private void Update()
+    {
+        cursorIcon.SetActive(itemMoving);
+        if (itemMoving)
+        {
+            cursorIcon.transform.position = Input.mousePosition;
+            cursorIcon.GetComponent<Image>().sprite = movingSlot.GetItem().icon;
+        }
+
+        if (Input.GetKeyDown(KeyCode.I)) {
+            if (inventoryOpen == false)
+            {
+                inventoryOpen = true;
+                inventoryPanel.SetActive(true);
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+            }
+            else if (inventoryOpen == true)
+            {
+                inventoryOpen = false;
+                inventoryPanel.SetActive(false);
+                Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+        }
+        if (Input.GetMouseButtonDown(0) && inventoryOpen == true) // When we click
+        {
+            if (itemMoving == true)
+            {
+                EndMove();
+            }
+            else
+            {
+                BeginMove();
+            }
+        }
+
+        // Hotbar slot selected with number keys
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            selectedSlot = 0;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            selectedSlot = 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            selectedSlot = 2;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            selectedSlot = 3;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            selectedSlot = 4;
+        }
+        hotbarSelector.transform.position = hotbarSlots[selectedSlot].transform.position;
+        selectedItem = items[selectedSlot + (hotbarSlots.Length * 3)].GetItem();
+    }
+
+    #region Inventory Utils
+    public void RefreshGUI()
+    {
+        for (int i = 0; i < slots.Length; i++)
+        {
+            try
+            {
+                slots[i].transform.GetChild(0).GetComponent<Image>().enabled = true;
+                slots[i].transform.GetChild(0).GetComponent<Image>().sprite = items[i].GetItem().icon;
+                if (items[i].GetItem().isStackable)
+                {
+                    slots[i].transform.GetChild(1).GetComponent<Text>().text = items[i].GetAmount().ToString();
+                }
+                else
+                {
+                    slots[i].transform.GetChild(1).GetComponent<Text>().text = "";
+                }
+            }
+            catch
+            {
+                slots[i].transform.GetChild(0).GetComponent<Image>().sprite = null;
+                slots[i].transform.GetChild(0).GetComponent<Image>().enabled = false;
+                slots[i].transform.GetChild(1).GetComponent<Text>().text = "";
+            }
+        }
+
+        RefreshHotbar();
+    }
+
+    public void RefreshHotbar()
+    {
+        for (int i = 0; i < hotbarSlots.Length; i++)
+        {
+            try
+            {
+                hotbarSlots[i].transform.GetChild(0).GetComponent<Image>().enabled = true;
+                hotbarSlots[i].transform.GetChild(0).GetComponent<Image>().sprite = items[i + (hotbarSlots.Length * 3)].GetItem().icon;
+                if (items[i + (hotbarSlots.Length * 3)].GetItem().isStackable)
+                {
+                    hotbarSlots[i].transform.GetChild(1).GetComponent<Text>().text = items[i + (hotbarSlots.Length * 3)].GetAmount().ToString();
+                }
+                else
+                {
+                    hotbarSlots[i].transform.GetChild(1).GetComponent<Text>().text = "";
+                }
+            }
+            catch
+            {
+                hotbarSlots[i].transform.GetChild(0).GetComponent<Image>().sprite = null;
+                hotbarSlots[i].transform.GetChild(0).GetComponent<Image>().enabled = false;
+                hotbarSlots[i].transform.GetChild(1).GetComponent<Text>().text = "";
+            }
+        }
+    }
+
+    public bool Add(ItemClass item, int amount)
+    {
+        SlotClass slot = Contains(item);
+        if (slot != null)
+        {
+            if (slot.GetItem().isStackable)
+            {
+                slot.AddAmount(1);
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (items[i].GetItem() == null)
+                {
+                    items[i].SetItem(item, amount);
+                    break;
+                }
+            }
+        }
+
+        RefreshGUI();
+        return true;
+    }
+
+    public bool Remove(ItemClass item)
+    {
+        SlotClass toRemove = Contains(item);
+        if (toRemove != null)
+        {
+            if (toRemove.GetAmount() > 1)
+            {
+                toRemove.RemoveAmount(1);
+            }
+            else
+            {
+                for (int i = 0; i < items.Length; i++)
+                {
+                    if (items[i].GetItem() == item)
+                    {
+                        items[i].Clear();
+                        break;
+                    }
+                }
+            }
+        }
+        else
+        {
+            return false;
+        }
+        RefreshGUI();
+        return true;
+    }
+
+    public SlotClass Contains(ItemClass item)
+    {
+        foreach (SlotClass slot in items)
+        {
+            if (slot.GetItem() == item)
+            {
+                return slot;
+            }
+        }
+        return null;
+    }
+    #endregion Inventory Utils
+
+    #region Moving Utils
+
+    private bool BeginMove()
+    {
+        originalSlot = FindClosestSlot();
+        if (originalSlot == null || originalSlot.GetItem() == null) // no item
+        {
+            return false;
+        }
+        movingSlot = new SlotClass(originalSlot);
+        originalSlot.Clear();
+        itemMoving = true;
+        RefreshGUI();
+        return true;
+    }
+    private SlotClass FindClosestSlot()
+    {
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if (Vector2.Distance(slots[i].transform.position, Input.mousePosition) <= 30)
+            {
+                return items[i];
+            }
+        }
+        return null;
+    }
+
+    private bool EndMove()
+    {
+        originalSlot = FindClosestSlot();
+        if (originalSlot == null)
+        {
+            Add(movingSlot.GetItem(),movingSlot.GetAmount());
+            movingSlot.Clear();
+            itemMoving = false;
+            return false;
+        }
+        if (originalSlot.GetItem() != null)
+        {
+            if (originalSlot.GetItem() == movingSlot.GetItem())
+            {
+                if (originalSlot.GetItem().isStackable)
+                {
+                    originalSlot.AddAmount(movingSlot.GetAmount());
+                    movingSlot.Clear();
+                }
+            }
+            else // Swap items
+            {
+                tempSlot = new SlotClass(originalSlot);
+                originalSlot.SetItem(movingSlot.GetItem(), movingSlot.GetAmount());
+                movingSlot.SetItem(tempSlot.GetItem(), tempSlot.GetAmount());
+                RefreshGUI();
+                return true;
+            }
+        }
+        else
+        {
+            originalSlot.SetItem(movingSlot.GetItem(),movingSlot.GetAmount());
+            movingSlot.Clear();
+        }
+        itemMoving = false;
+        RefreshGUI();
+        return true;
+    }
+
+    #endregion Moving Utils
+}
---
+++ Assets/Scripts/Items/ItemClass.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using UnityEngine;
+
+public class ItemClass : ScriptableObject
+{
+    [Header("Item")]
+    public string itemName;
+    public Sprite icon;
+    public GameObject model;
+    public bool isStackable;
+
+    public virtual void Use(PlayerController player)
+    {
+        Debug.Log("Used item");
+    }
+    public virtual ItemClass getItem() { return this; }
+    public virtual FoodClass getFood() { return null; }
+    public virtual MeleeClass getMelee() { return null; }
+    public virtual GunClass getGun() { return null; }
+
+    public virtual GameObject getModel() { return model; }
+}

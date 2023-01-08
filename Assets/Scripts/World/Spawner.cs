0dfb67b6b7c5699b07bcda9669a87a1cd501a923
JasonT962 <78263363+JasonT962@users.noreply.github.com>
2023-01-08 21:12:25 +0000

    Fixed spawner rotation + empty parent for zombies

--- Assets/Scripts/World/Spawner.cs
+++ Assets/Scripts/World/Spawner.cs
@@ -8,19 +8,21 @@ public class Spawner : MonoBehaviour
     [SerializeField] GameObject medZombie;
     [SerializeField] GameObject fastZombie;
 
+    [SerializeField] GameObject ZombieList;
+
     public void Spawn(string zombieType)
     {
         if (zombieType == "big")
         {
-            Instantiate(bigZombie, transform.position, transform.rotation);
+            Instantiate(bigZombie, transform.position, transform.rotation, ZombieList.transform);
         }
         else if (zombieType == "med")
         {
-            Instantiate(medZombie, transform.position, transform.rotation);
+            Instantiate(medZombie, transform.position, transform.rotation, ZombieList.transform);
         }
         else if (zombieType == "fast")
         {
-            Instantiate(fastZombie, transform.position, transform.rotation);
+            Instantiate(fastZombie, transform.position, transform.rotation, ZombieList.transform);
         }
         else
         {
